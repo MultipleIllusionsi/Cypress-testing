0e8f062b3cf0916a5f8147b5aaf17df64a2a5f4f
MultipleIllusionsi <seregaserega06@gmail.com>
2020-03-22 23:11:43 +0500

    add more scenarios

--- cypress/integration/cucumber-tests/login/loginPage.js
+++ cypress/integration/cucumber-tests/login/loginPage.js
@@ -2,22 +2,29 @@ const URL = "http://zero.webappsecurity.com/login.html";
 const USERNAME_INPUT = "#user_login";
 const PASSWORD_INPUT = "#user_password";
 const SUBMIT_BUTTON = 'input[name="submit"]';
+const ERROR_MESSAGE = ".alert-error";
 
 class LoginPage {
   static visit() {
     cy.visit(URL);
   }
 
-  static fullUsername(name) {
+  static fillUsername(name) {
     cy.get(USERNAME_INPUT).type(name);
   }
 
-  static fullPassword(name) {
+  static fillPassword(name) {
     cy.get(PASSWORD_INPUT).type(name);
   }
 
   static submit() {
     cy.get(SUBMIT_BUTTON).click();
+  }
+
+  static shouldShowErrorMessage() {
+    cy.get(ERROR_MESSAGE).contains(
+      "Login and/or password are wrong"
+    );
   }
 }
 
--- cypress/integration/cucumber-tests/login/loginSteps.js
+++ cypress/integration/cucumber-tests/login/loginSteps.js
@@ -9,12 +9,22 @@ Given("I open login page", () => {
   LoginPage.visit();
 });
 
-When("I submit login", () => {
-  LoginPage.fullUsername("username");
-  LoginPage.fullPassword("password");
+When("I fill username with {string}", username => {
+  LoginPage.fillUsername(username);
+});
+
+When("I fill password with {string}", password => {
+  LoginPage.fillPassword(password);
+});
+
+When("I click on submit login", () => {
   LoginPage.submit();
 });
 
 Then("I should see homepage", () => {
   cy.get("#account_summary_tab").should("be.visible");
+});
+
+Then("I should see error message", () => {
+  LoginPage.shouldShowErrorMessage();
 });
